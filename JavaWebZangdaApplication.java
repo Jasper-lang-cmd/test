fcb9f39ae42aaa0296f15f28cd32219d46ac10ad
Lll <2996215002@qq.com>
2024-06-26 21:43:14 +0800

    666

---
+++ JavaWebZangdaApplication.java
@@ -0,0 +1,13 @@
+package cn.lilulu.java_web_zangda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebZangdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebZangdaApplication.class, args);
+    }
+
+}
