2ef61720b4c6825b73f1c1f343a81e88b2ae1816
Lll <2996215002@qq.com>
2024-07-06 18:43:27 +0800

    2024 7 6 1

---
+++ vue3/vue-base/src/App.vue
@@ -0,0 +1,43 @@
+<!-- Vue使用一种基于HTML的模板语法，
+      让我们能够声明式地将其组件实例地数据绑定到呈现的DOM上。
+      所有的Vue模板都是语法层面合法的HTML，可以被符合规范的浏览器和HTML解析器解析。
+-->
+
+<!-- 最基本的数据绑定形式 : 文本插值  -->
+<!-- 它使用的是“Mustache”语法 : 即双大括号 -->
+
+<!-- 使用JavaScript表达式 -->
+<!-- 每个绑定仅支持 单一表达式 ，也就是一段能够被求值的JavaScript代码 -->
+<!-- 一个简单的判断方法是是否可以合法地写在 return 后面 -->
+
+<!-- 原始HTML : 
+          双大括号将会将数据插值为纯文本，而不是HTML。 
+          若想插入HTML，需要使用 v-html 指令。
+ -->
+<script>
+export default {
+  data() {
+    return {
+      msg: "op",
+      number: 10,
+      ok: true,
+      message: "大家好",
+      rawHTML: "<a href='https://itbaizhan.com'>百战程序猿</a>",
+    };
+  },
+};
+</script>
+
+<template>
+  <h3>模板语法</h3>
+  <p>{{ msg }}</p>
+
+  <p>{{ number + 1 }}</p>
+  <p>{{ ok ? "Yes" : "No" }}</p>
+  <p>{{ message.split("").reverse().join("") }}</p>
+  <!-- 这是一个语句，而非表达式 ,另外条件控制也不行，要用三目运算符-->
+  <!-- <p>{{ var a = 1 }}</p> -->
+
+  <p>{{ rawHTML }}</p>
+  <p v-html="rawHTML"></p>
+</template>
---
+++ vue3/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<!-- 属性绑定 -->
+<!-- 
+     双大括号不能在HTML attributes 中使用。 
+     想要响应式地绑定一个 attributes ，应该使用 v-bind 指令。
+-->
+
+<!-- 注意 : 
+      v-bind 指令指示Vue的元素的 id attribute与组件的 dynamicld 属性保持一致。
+      如果绑定的值是 null 或者 undefined ,那么该 attribute 将会从渲染的元素上移除。
+      简写形式 : 冒号 即 v-bind 等价于 :
+-->
+
+<!-- 布尔型（Attribute）
+      依据true / false 值来决定 attribute 是否应该存在于该元素上，disabled 就是最常见的例子。 
+-->
+<script>
+export default {
+  data() {
+    return {
+      dynamicClass: "appclass",
+      dynamicId: "appid",
+      dynamicTitle: null,
+      isButtonDisabled: true,
+      // 包含多个attribute的JavaScript对象
+      objectofAttributes: {
+        class: "appclass",
+        id: "appid",
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    v-bind:id="dynamicId"
+    v-bind:class="dynamicClass"
+    v-bind:title="dynamicTitle"
+  >
+    测试
+  </div>
+
+  <!-- 布尔型 -->
+  <button :disabled="isButtonDisabled">button</button>
+
+  <!-- 动态绑定多个值 -->
+  <div :="objectofAttributes">测试02</div>
+</template>
+
+<style>
+#appid {
+  color: red;
+  font-size: 30px;
+}
+</style>
