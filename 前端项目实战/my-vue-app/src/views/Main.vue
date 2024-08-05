72010357e8153b1636fde9461d605db006fc115c
Lll <2996215002@qq.com>
2024-08-05 23:54:28 +0800

    2024 8 5 1

---
+++ 前端项目实战/my-vue-app/src/views/Main.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <p>我是Main组件</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 前端项目实战/my-vue-app/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 这个 resolve 是添加的别名。
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: "/src",
+      },
+    ],
+  },
+});
